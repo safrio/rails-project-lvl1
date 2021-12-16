d201f0ba28578c1940bb60e921637168497c4c19
Ivan Safronov <ivan.safronov@mtxc.eu>
2021-12-16 16:06:59 +0500

    update tag styling )

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'error'
+
 module HexletCode
   class Tag
     SINGLE_TAGS = %i[br hr img input].freeze
 
     class << self
       def build(tag, *options, &block)
-        raise 'Tag param is not a string' unless tag.instance_of?(String)
-        raise 'Tag param is empty' if tag.to_s.strip.empty?
+        raise HexletCodeError, "Tag param is not a string: #{tag}" unless tag.instance_of?(String)
+        raise HexletCodeError, 'Tag param is empty' if tag.to_s.strip.empty?
 
         is_single = SINGLE_TAGS.include? tag.to_sym
-        raise 'Can\'t generate single tag with block' if is_single && block_given?
+        raise HexletCodeError, 'Can\'t generate single tag with block' if is_single && block_given?
 
         return single_tag(tag, options) if is_single
 
@@ -21,11 +23,13 @@ module HexletCode
 
       def paired_tags(tag, options, block) = format_paired_tags(tag, render_options(options), block&.call)
 
-      def render_options(options) = options.map { |option| option.map { |key, value| %Q[ #{key}="#{value}"] } }.join
+      def render_options(options) = options.map { |option| option.map { |key, value| %( #{key}="#{value}") } }.join
 
       def format_single_tag(tag, options) = "<#{tag}#{options}>"
 
-      def format_paired_tags(tag, options, block, splitter = '') = "<#{tag}#{options}>#{splitter}#{block}</#{tag}>"
+      def format_paired_tags(tag, options, block, splitter = '')
+        "<#{tag}#{options}>#{splitter}#{block}#{block =~ /#{splitter}/ ? splitter : ''}</#{tag}>"
+      end
     end
   end
 end
