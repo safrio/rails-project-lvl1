75822ce331311617805f691d424f78a2b9af8b60
Ivan Safronov <ivan.safronov@mtxc.eu>
2021-12-16 16:07:36 +0500

    add more form_for tests

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -5,8 +5,8 @@ require 'minitest/autorun'
 
 class HexletCodeText < Minitest::Test
   def setup
-    user = Struct.new(:name, :job, keyword_init: true)
-    @user = user.new name: 'rob'
+    user = Struct.new(:name, :job, :gender, keyword_init: true)
+    @user = user.new name: 'rob', job: 'hexlet', gender: 'm'
   end
 
   def test_that_it_has_a_version_number
@@ -38,20 +38,20 @@ class HexletCodeText < Minitest::Test
     assert_equal test_instance, '<label for="email">Email</label>'
   end
 
-  def test_incorrect_single_tag_with_block
-    assert_raises RuntimeError do
+  def test_error_single_tag_with_block
+    assert_raises HexletCode::HexletCodeError do
       HexletCode::Tag.build('hr', for: 'email') { 'Email' }
     end
   end
 
-  def test_incorrect_empty_tag
-    assert_raises RuntimeError do
+  def test_error_empty_tag
+    assert_raises HexletCode::HexletCodeError do
       HexletCode::Tag.build('')
     end
   end
 
-  def test_incorrect_tag_type
-    assert_raises RuntimeError do
+  def test_error_tag_type
+    assert_raises HexletCode::HexletCodeError do
       HexletCode::Tag.build({})
     end
   end
@@ -64,5 +64,27 @@ class HexletCodeText < Minitest::Test
   def test_form_for_with_url
     test_instance = HexletCode.form_for @user, url: '/users'
     assert_equal test_instance, %(<form action="/users" method="post">\n</form>)
+  end
+
+  def test_form_for_with_block
+    test_instance = HexletCode.form_for @user do |f|
+      f.input :name
+      f.input :job, as: :text
+    end
+    assert_equal test_instance, %(<form action="#" method="post">
+<input name="name" type="text" value="rob">
+<textarea cols="20" rows="40" name="job">hexlet</textarea>
+</form>)
+  end
+
+  def test_error_form_for_with_url_and_block
+    assert_raises NameError do
+      HexletCode.form_for @user, url: '/users' do |f|
+        f.input :name
+        f.input :job, as: :text
+        f.input :age
+        f.submit
+      end
+    end
   end
 end
